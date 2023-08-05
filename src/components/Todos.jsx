a0bae568fe196a48d2d6e62e96cf648c8851f301
samery46 <syamme@ymail.com>
2023-08-06 00:01:51 +0700

    Add CSS to components

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,13 +3,19 @@ import TodoItem from './TodoItem' // Lakukan import
 
 const Todos = ({ todos }) => {
     return (
-      <div>
+      <div style={styles.container}>
         {todos.map((todo) => {
-          // Berikan datanya ke TodoItem
           return <TodoItem key={todo.id} todo={todo} />
         })}
       </div>
     )
+  }
+
+  const styles = {
+    container: {
+      width: '40%',
+      margin: '0 auto',
+    },
   }
 
 export default Todos
