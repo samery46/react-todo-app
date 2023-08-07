f1724083699a526fa638fda81f6cd64078872897
samery46 <syamme@ymail.com>
2023-08-07 22:25:02 +0700

    Develop delete todo fiture

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -19,25 +19,36 @@ const TodoItem = ({ todo, toggleCompleted }) => {
         onChange={() => toggleCompleted(todo.id)}
       />
       <p style={getTodoTitleStyle()}>{todo.title}</p>
+      {/* Tambahkan sebuah button di sini */}
+      <button style={styles.button}>x</button>      
     </div>
   )
   }
 
-  const styles = {
-    todoItem: {
-      border: '2px solid #f4f4f4',
-      fontSize: '24px',
-      // Tambahkan styles di bawah ini
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    // Tambahkan styles di bawah ini
-    checkbox: {
-      marginRight: '10px',
-      height: '18px',
-      width: '18px',
-    },
+  // Salin CSS yang terbaru CSS di bawah ini
+const styles = {
+  todoItem: {
+    border: '2px solid #f4f4f4',
+    fontSize: '24px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0 20px',
+  },
+  checkbox: {
+    height: '18px',
+    width: '18px',
+  },
+  button: {
+    backgroundColor: '#BB0000',
+    color: '#fff',
+    height: '30px',
+    width: '30px',
+    borderRadius: '100%',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
+  },
   }
 
 export default TodoItem
